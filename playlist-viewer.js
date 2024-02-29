7cc84a1ed0e5e568edc61961e2ca466acebfe103
ammonharps-git <ammonharps@gmail.com>
2024-02-28 17:22:42 -0700

    Added removing talks functionality

--- playlist-viewer.js
+++ playlist-viewer.js
@@ -1,4 +1,7 @@
 
+// PLACEHOLDER for database values
+let talkList = ['His Grace is Sufficient by Brad Wilcox', 'The Character of Christ by David A. Bednar', 'Think Celesital by Russel M. Nelson']
+
 // Display the username at the top of welcome banner
 document.addEventListener('DOMContentLoaded', function () {
 
@@ -9,3 +12,22 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('playlistName').innerText = playlistName;
 });
 
+function removeTalk(talkName) {
+    // Retrieve the username from the URL
+    const playlistName = getQueryParam('playlistName');
+
+    // --------PLACEHOLDER--------- 
+    // talkList = database values
+    // --------END PLACEHOLDER-----
+
+    // Check if the talkName is in the talkList
+    const indexToRemove = talkList.indexOf(talkName);
+
+    if (indexToRemove !== -1) {
+        // Remove the talk if found
+        talkList.splice(indexToRemove, 1);
+        console.log(`${talkName} removed from ${playlistName}.`);
+    } else {
+        console.log(`${talkName} not found in ${playlistName}.`);
+    }
+}
