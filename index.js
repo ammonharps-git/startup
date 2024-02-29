9acf803e46fef6fe0bd5cddebbb4a415e3caddc9
ammonharps-git <ammonharps@gmail.com>
2024-02-28 22:15:36 -0700

    more re-doing with localstorage

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 
 function login() {
 
+    // Get the username and password that the user entered
+    var username = document.getElementById("typeUsernameX").value;
+    var password = document.getElementById("typePasswordX").value;
+
     // PLACEHOLDER
     // Set the local storage initial value
-    if (localStorage.getItem('valid-credentials')) {
+    if (!localStorage.getItem('valid-credentials')) {
         localStorage.setItem('valid-credentials', JSON.stringify([]));
     }
     // Retrieve valid credentials from local storage
@@ -16,10 +20,7 @@ function login() {
     } catch (error) {
         console.error('Error parsing JSON from localStorage:', error);
     }
-
-    // Get the username and password that the user entered
-    var username = document.getElementById("typeUsernameX").value;
-    var password = document.getElementById("typePasswordX").value;
+    console.log(validCredentials);
 
     // Check if the provided username and password match any valid credentials
     const isValidUser = validCredentials.some(cred => cred.username === username && cred.password === password);
@@ -51,7 +52,7 @@ function register() {
 
     // PLACEHOLDER
     // Set the local storage initial value
-    if (localStorage.getItem('valid-credentials')) {
+    if (!localStorage.getItem('valid-credentials')) {
         localStorage.setItem('valid-credentials', JSON.stringify([]));
     }
     // Retrieve valid credentials from local storage
@@ -64,6 +65,7 @@ function register() {
     } catch (error) {
         console.error('Error parsing JSON from localStorage:', error);
     }
+    console.log(validCredentials);
 
     // Check if the provided username and password match any valid credentials
     const sameUsername = validCredentials.some(cred => cred.username === username);
@@ -83,8 +85,10 @@ function register() {
             errorMessageElement.innerText = ''; // Clear the message for the next attempt
         }, 3000); // Hide the message after 3 secondsc
     } else {
-        validCredentials.push({'username': username, 'password': password})
-        localStorage.setItem('array', JSON.stringify(validCredentials));
+        validCredentials.push({'username': username, 'password': password});
+        localStorage.setItem('valid-credentials', JSON.stringify(validCredentials));
+        localStorage.setItem(username, JSON.stringify({'name': username, 'playlists': []}));
+        console.log("Registered and logged in as", username);
         // Redirect to 'my-playlists.html' since login is successful
         window.location.href = `my-playlists.html?username=${encodeURIComponent(username)}`;
     }
